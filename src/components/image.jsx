6db08f59c92703231e0c6b228120babdbfb38560
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-10-04 11:55:48 -0300

    cambio de imagen autos

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,12 +3,21 @@ import React from "react";
 export const Image = ({ title, image }) => {
   return (
     <div className="portfolio-item">
-      <div className="hover-bg">
+      <div className="hover-bg" style={{ position: "relative", overflow: "hidden" }}>
         <a href={image} title={title} data-lightbox-gallery="gallery1">
           <div className="hover-text">
             <h4>{title}</h4>
           </div>
-          <img src={image} className="img-responsive" alt={title} />
+          <img
+            src={image}
+            className="img-responsive"
+            alt={title}
+            style={{
+              width: "100%",
+              height: "268.86px", 
+              objectFit: "cover", 
+            }}
+          />
         </a>
       </div>
     </div>
